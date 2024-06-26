eac665544e272e3839cc81dbdb72593117fa5b31
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-26 16:21:33 -0300

    refactor(db): remove type generics from query function

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,6 @@ import { Pool } from "pg";
 
 const pool = new Pool();
 
-export function query<T>(text: string, params: any[]) {
-  return pool.query<T[]>(text, params);
+export function query(text: string, params: any[]) {
+  return pool.query(text, params);
 }
