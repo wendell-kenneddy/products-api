70c727a951ca773366c6b5e82e9ba039eac12f60
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-26 16:23:25 -0300

    feat(index.ts): initialize helmet properly and add error handler middleware to app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import helmet from "helmet";
 import { router } from "./routes";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 
 const port = process.env.PORT;
 const app = express();
 
-app.use(helmet);
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
+app.use(errorHandlerMiddleware);
 
 app.listen(port, () => {
   console.log(`[server]: application started on http://localhost:${port}`);
