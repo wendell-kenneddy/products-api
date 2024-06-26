8f4ead53a1094a1058e0f4d2e56983d71f108806
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-26 16:22:49 -0300

    feat(routes): add users related routes using UsersController

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,14 @@
 import Router from "express-promise-router";
+import { UsersController } from "../controllers/UsersController";
+import { PostgresUsersRepository } from "../repositories/PostgresUsersRepository";
 
 const router = Router();
+const usersController = new UsersController(new PostgresUsersRepository());
+
+router.get("/users", usersController.getMany);
+router.get("/users/:id", usersController.getOne);
+router.post("/users", usersController.create);
+router.delete("/users/:id", usersController.deleteOne);
+router.put("/users/:id", usersController.updateOne);
 
 export { router };
