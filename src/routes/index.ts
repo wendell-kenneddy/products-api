5836f01483ec0cdcebd2563711b7b16a8d68fd30
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-27 18:31:12 -0300

    feat(routes): add more routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,23 @@
 import Router from "express-promise-router";
 import { UsersController } from "../controllers/UsersController";
-import { PostgresUsersRepository } from "../repositories/PostgresUsersRepository";
+import { CategoriesController } from "../controllers/CategoriesController";
+import { ProductsController } from "../controllers/ProductsController";
 
 const router = Router();
-const usersController = new UsersController(new PostgresUsersRepository());
+const usersController = new UsersController();
+const categoriesController = new CategoriesController();
+const productsController = new ProductsController();
 
 router.get("/users", usersController.getMany);
 router.get("/users/:id", usersController.getOne);
 router.post("/users", usersController.create);
 router.delete("/users/:id", usersController.deleteOne);
-router.put("/users/:id", usersController.updateOne);
+
+router.get("/categories", categoriesController.getMany);
+router.get("/categories/:id", categoriesController.getOne);
+router.post("/categories", categoriesController.create);
+router.delete("/categories/:id", categoriesController.deleteOne);
+
+router.post("/products", productsController.create);
 
 export { router };
