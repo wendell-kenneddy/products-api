bf9175a07499f6f01a3087d2c1bc47644b2885e7
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-27 18:18:44 -0300

    feat(index.ts): use body-parser middleware

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import helmet from "helmet";
 import { router } from "./routes";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
+import bodyParser from "body-parser";
 
 const port = process.env.PORT;
 const app = express();
 
 app.use(helmet());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.use(errorHandlerMiddleware);
 
