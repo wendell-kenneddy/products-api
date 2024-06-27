4297b7488da1ef2d8264835c362abad97b76e3a7
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-27 18:22:49 -0300

    feat(services): add products related services entry point

---
+++ src/services/products/index.ts
@@ -0,0 +1,32 @@
+import { ObjectSchema, number, object, string } from "yup";
+
+export interface Product {
+  productName: string;
+  productImageUrl?: string;
+  productDescription: string;
+  productPrice: number;
+  productStock: number;
+}
+
+export interface ProductWithID extends Product {
+  productID: string;
+}
+
+export interface NormalizedProduct {
+  product_id: string;
+  product_name: string;
+  product_image_url?: string;
+  product_description: string;
+  product_price: number;
+  product_stock: number;
+}
+
+export const productSchema: ObjectSchema<Product> = object({
+  productName: string().required("Product name required."),
+  productImageUrl: string().optional(),
+  productDescription: string().required("Product description required."),
+  productPrice: number()
+    .required("Product price required.")
+    .positive("Product price must be positive."),
+  productStock: number().required("Product stock required."),
+});
