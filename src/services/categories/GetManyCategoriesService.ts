6423d469aced8a47f510d54189e4d7dec55a29d8
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-27 18:28:08 -0300

    feat(services): add GetManyCategoriesService

---
+++ src/services/categories/GetManyCategoriesService.ts
@@ -0,0 +1,13 @@
+import { CategoryWithID } from ".";
+import { query } from "../../db";
+import { DataParser } from "../../utils/DataParser";
+
+export class GetManyCategoriesService {
+  async execute(condition: string): Promise<CategoryWithID[]> {
+    const result = await query(`SELECT * FROM categories ${condition}`, []);
+    const categories: CategoryWithID[] = result.rows.map((c) =>
+      new DataParser().parseNormalizedCategory(c)
+    );
+    return categories;
+  }
+}
