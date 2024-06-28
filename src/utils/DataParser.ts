4eea132ffa8e4e2a8836349f5a7a42281d55ccdf
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-28 17:56:20 -0300

    feat(utils): add more parsing functions to DataParser

--- src/utils/DataParser.ts
+++ src/utils/DataParser.ts
@@ -1,4 +1,11 @@
 import { CategoryWithID, NormalizedCategory } from "../services/categories";
+import { NormalizedProductWithIDAndQuantity, ProductWithIDAndQuantity } from "../services/orders";
+import {
+  NormalizedProduct,
+  NormalizedProductWithCategory,
+  ProductWithCategory,
+  ProductWithID,
+} from "../services/products";
 import { NormalizedUser, UserWithID } from "../services/users";
 
 export class DataParser {
@@ -22,6 +29,57 @@ export class DataParser {
     return {
       categoryID: category_id,
       categoryName: category_name,
+    };
+  }
+
+  parseNormalizedProduct({
+    product_id,
+    product_name,
+    product_description,
+    product_image_url,
+    product_price,
+    product_stock,
+  }: NormalizedProduct): ProductWithID {
+    return {
+      productID: product_id,
+      productName: product_name,
+      productDescription: product_description,
+      productImageUrl: product_image_url,
+      productPrice: product_price,
+      productStock: product_stock,
+    };
+  }
+
+  parseNormalizedProductWithCategory(data: NormalizedProductWithCategory): ProductWithCategory {
+    return {
+      productID: data.product_id,
+      productName: data.product_name,
+      productDescription: data.product_description,
+      productImageUrl: data.product_image_url,
+      productPrice: data.product_price,
+      productStock: data.product_stock,
+      categoryID: data.category_id,
+      categoryName: data.category_name,
+    };
+  }
+
+  parseNormalizedProductWithIDAndQuantity({
+    product_id,
+    product_name,
+    product_description,
+    product_image_url,
+    product_price,
+    product_stock,
+    quantity,
+  }: NormalizedProductWithIDAndQuantity): ProductWithIDAndQuantity {
+    return {
+      productID: product_id,
+      productName: product_name,
+      productDescription: product_description,
+      productImageUrl: product_image_url,
+      productPrice: product_price,
+      productStock: product_stock,
+      quantity,
     };
   }
 }
