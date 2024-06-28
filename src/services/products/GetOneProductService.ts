5cb189ab47e29112a3fb3d95762444c5ed36b2df
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-28 17:56:44 -0300

    feat(services): add GetOneProductService

---
+++ src/services/products/GetOneProductService.ts
@@ -0,0 +1,39 @@
+import { ProductWithID } from ".";
+import { getClient } from "../../db";
+import { DataParser } from "../../utils/DataParser";
+import { uuidSchema } from "../../utils/uuidSchema";
+import { CategoryWithID } from "../categories";
+
+export class GetOneProductService {
+  async execute(id: string) {
+    await uuidSchema.validate(id);
+    const client = await getClient();
+    const parser = new DataParser();
+    let data: { product: ProductWithID; categories: CategoryWithID[] } | {} = {};
+
+    // no catch block so it gets handled by the error handler middleware
+    try {
+      const productQuery = await client.query("SELECT * FROM products WHERE product_id = $1", [id]);
+
+      if (productQuery.rowCount) {
+        const categoriesQuery = await client.query(
+          `SELECT 
+             c.category_id,
+             c.category_name
+           FROM product_categories pc
+           INNER JOIN categories c ON pc.category_id = c.category_id
+           WHERE pc.product_id = $1
+        `,
+          [id]
+        );
+        const productWithID = parser.parseNormalizedProduct(productQuery.rows[0]);
+        const categoriesWithID = categoriesQuery.rows.map((c) => parser.parseNormalizedCategory(c));
+        data = { product: productWithID, categories: categoriesWithID };
+      }
+    } finally {
+      client.release();
+    }
+
+    return { ...data };
+  }
+}
