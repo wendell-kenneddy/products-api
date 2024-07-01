2d912de811d4c065bd2483cd891015f9bfe35674
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-01 17:19:14 -0300

    feat(controllers): add OrdersController

---
+++ src/controllers/OrdersController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { CreateOrderProductService } from "../services/orders/CreateOrderProductService";
+import { GetOrderProductsService } from "../services/orders/GetOrderProductsService";
+import { DeleteOrderProductService } from "../services/orders/DeleteOrderProductService";
+
+export class OrdersController {
+  createOrderProduct = async (req: Request, res: Response) => {
+    const customerID = req.params.customerID;
+    const productID = req.body.productID;
+    await new CreateOrderProductService().execute(customerID, productID);
+    res.status(200).json({
+      message: "Product successfully added to order.",
+    });
+  };
+
+  getOrderProducts = async (req: Request, res: Response) => {
+    const customerID = req.params.customerID;
+    const orderProducts = await new GetOrderProductsService().execute(customerID);
+    res.status(200).json({ data: orderProducts });
+  };
+
+  deleteOrderProduct = async (req: Request, res: Response) => {
+    const orderProductID = req.body.orderProductID;
+    await new DeleteOrderProductService().execute(orderProductID);
+    res.status(200).json({ message: "Product successfully deleted from order." });
+  };
+}
