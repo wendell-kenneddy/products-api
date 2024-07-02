326cca2ae8bedaa8bf2528e1c3e9ebe6188318b1
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-02 17:40:37 -0300

    feat(routes): add auth related middlewares

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import { UsersController } from "../controllers/UsersController";
 import { CategoriesController } from "../controllers/CategoriesController";
 import { ProductsController } from "../controllers/ProductsController";
 import { OrdersController } from "../controllers/OrdersController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { withoutAuthMiddleware } from "../middlewares/withoutAuthMiddleware";
 
 const router = Router();
 const usersController = new UsersController();
@@ -10,30 +12,37 @@ const categoriesController = new CategoriesController();
 const productsController = new ProductsController();
 const ordersController = new OrdersController();
 
-router.get("/users", usersController.getMany);
-router.get("/users/:id", usersController.getOne);
-router.post("/users", usersController.create);
-router.delete("/users/:id", usersController.deleteOne);
+router.get("/users", authMiddleware, usersController.getMany);
+router.get("/users/profile", authMiddleware, usersController.getOne);
+router.post("/users", authMiddleware, usersController.create);
+router.post("/users/login", withoutAuthMiddleware, usersController.login);
+router.post("/users/signup", withoutAuthMiddleware, usersController.signup);
+router.delete("/users", authMiddleware, usersController.deleteOne);
 
 router.get("/categories", categoriesController.getMany);
-router.get("/categories/:id", categoriesController.getOne);
-router.post("/categories", categoriesController.create);
-router.post("/categories/add-to-product", categoriesController.addCategoryToExistingProduct);
+router.get("/categories/:categoryID", categoriesController.getOne);
+router.post("/categories", authMiddleware, categoriesController.create);
+router.post(
+  "/categories/add-to-product",
+  authMiddleware,
+  categoriesController.addCategoryToExistingProduct
+);
 router.delete(
   "/categories/delete-from-product",
+  authMiddleware,
   categoriesController.deleteCategoryFromExistingProduct
 );
-router.delete("/categories/:id", categoriesController.deleteOne);
+router.delete("/categories/:categoryID", authMiddleware, categoriesController.deleteOne);
 
 router.get("/products", productsController.getMany);
-router.get("/products/:id", productsController.getOne);
-router.post("/products", productsController.create);
-router.put("/products/:productID", productsController.updateOne);
-router.delete("/products/:id", productsController.deleteOne);
+router.get("/products/:productID", productsController.getOne);
+router.post("/products", authMiddleware, productsController.create);
+router.put("/products/:productID", authMiddleware, productsController.updateOne);
+router.delete("/products/:productID", authMiddleware, productsController.deleteOne);
 
-router.get("/orders/:customerID", ordersController.getOrderProducts);
-router.post("/orders/:customerID", ordersController.createOrderProduct);
-router.post("/orders/:customerID/checkout", ordersController.checkout);
-router.delete("/orders/delete-order-product", ordersController.deleteOrderProduct);
+router.get("/orders", authMiddleware, ordersController.getOrderProducts);
+router.post("/orders/product", authMiddleware, ordersController.createOrderProduct);
+router.post("/orders/checkout", authMiddleware, ordersController.checkout);
+router.delete("/orders/product", authMiddleware, ordersController.deleteOrderProduct);
 
 export { router };
