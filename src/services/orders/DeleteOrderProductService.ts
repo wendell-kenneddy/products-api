5c20ac06014dc5229e503efe2bdf9973ef7a1884
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-02 17:37:57 -0300

    feat(DeleteOrderProductService): add check for matching customer_id

--- src/services/orders/DeleteOrderProductService.ts
+++ src/services/orders/DeleteOrderProductService.ts
@@ -2,8 +2,16 @@ import { query } from "../../db";
 import { uuidSchema } from "../../utils/uuidSchema";
 
 export class DeleteOrderProductService {
-  async execute(orderProductID: string) {
+  async execute(customerID: string, orderProductID: string) {
     uuidSchema.validate(orderProductID);
-    await query("DELETE FROM order_products WHERE order_product_id = $1", [orderProductID]);
+    uuidSchema.validate(customerID);
+    await query(
+      `WITH o_query AS (SELECT order_id FROM orders o WHERE o.customer_id = $1)
+       DELETE FROM order_products op
+       WHERE op.order_id = (SELECT order_id FROM o_query)
+       AND op.order_product_id = $2
+      `,
+      [customerID, orderProductID]
+    );
   }
 }
