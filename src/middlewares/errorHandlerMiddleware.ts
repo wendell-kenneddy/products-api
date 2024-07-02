4e6fe8c39720bb3e736955dd5e98be514dab3740
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-02 13:19:11 -0300

    feat(errorHandlerMiddleware): add custom message to handle products not in stock

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -5,7 +5,12 @@ export function errorHandlerMiddleware(err: any, req: Request, res: Response, ne
   console.log(err);
   let message = "Something went wrong.";
 
-  if (err instanceof ValidationError || err instanceof Error) message = err.message;
+  if (err instanceof ValidationError || err instanceof Error) {
+    err.message ==
+    'new row for relation "products" violates check constraint "products_product_stock_check"'
+      ? (message = "One or more product(s) contained in the order are currently not in stock.")
+      : (message = err.message);
+  }
 
   return res.status(500).json({ message });
 }
