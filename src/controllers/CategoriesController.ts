d8dfd1303d5033d0c0f69904e655d064925ec555
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-02 17:40:14 -0300

    feat(CategoriesController): handle auth using JWT on protected routes

--- src/controllers/CategoriesController.ts
+++ src/controllers/CategoriesController.ts
@@ -6,9 +6,12 @@ import { queryPageSchema } from "../utils/queryPageSchema";
 import { GetManyCategoriesService } from "../services/categories/GetManyCategoriesService";
 import { CreateProductCategoryService } from "../services/categories/AddCategoryToProductService";
 import { DeleteCategoryFromExistingProductService } from "../services/categories/DeleteCategoryFromExistingProductService";
+import { JwtPayload } from "jsonwebtoken";
 
 export class CategoriesController {
   create = async (req: Request, res: Response) => {
+    const payload: JwtPayload = (req as any).jwtPayload;
+    if (Number(payload.userAccessLevel) < 2) res.status(403).json({ message: "Forbidden access." });
     const data = req.body;
     const category = await new CreateCategoryService().execute(data);
     return res.status(200).json({
@@ -17,8 +20,8 @@ export class CategoriesController {
   };
 
   getOne = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const category = await new GetOneCategoryService().execute(id);
+    const categoryID = req.params.categoryID;
+    const category = await new GetOneCategoryService().execute(categoryID);
     return res.status(200).json({ data: category });
   };
 
@@ -31,20 +34,26 @@ export class CategoriesController {
   };
 
   deleteOne = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    await new DeleteOneCategoryService().execute(id);
+    const payload: JwtPayload = (req as any).jwtPayload;
+    if (Number(payload.userAccessLevel) < 2) res.status(403).json({ message: "Forbidden access." });
+    const categoryID = req.params.categoryID;
+    await new DeleteOneCategoryService().execute(categoryID);
     return res.status(200).json({
       message: "Category successfully deleted.",
     });
   };
 
   addCategoryToExistingProduct = async (req: Request, res: Response) => {
+    const payload: JwtPayload = (req as any).jwtPayload;
+    if (Number(payload.userAccessLevel) < 2) res.status(403).json({ message: "Forbidden access." });
     const { productID, categoryID } = req.body;
     await new CreateProductCategoryService().execute(productID, categoryID);
     return res.json({ message: "Category successfully added to product." });
   };
 
   deleteCategoryFromExistingProduct = async (req: Request, res: Response) => {
+    const payload: JwtPayload = (req as any).jwtPayload;
+    if (Number(payload.userAccessLevel) < 2) res.status(403).json({ message: "Forbidden access." });
     const { productID, categoryID } = req.body;
     await new DeleteCategoryFromExistingProductService().execute(productID, categoryID);
     res.status(200).json({ message: "Category successfully removed." });
