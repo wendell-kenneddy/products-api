f3d9f1dd613fc5a5d0307392be95b4cd4040e89f
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-02 17:39:06 -0300

    feat(OrdersController): handle auth using JWT on protected routes

--- src/controllers/OrdersController.ts
+++ src/controllers/OrdersController.ts
@@ -3,10 +3,12 @@ import { CreateOrderProductService } from "../services/orders/CreateOrderProduct
 import { GetOrderProductsService } from "../services/orders/GetOrderProductsService";
 import { DeleteOrderProductService } from "../services/orders/DeleteOrderProductService";
 import { OrderCheckoutService } from "../services/orders/OrderCheckoutService";
+import { JwtPayload } from "jsonwebtoken";
 
 export class OrdersController {
   createOrderProduct = async (req: Request, res: Response) => {
-    const customerID = req.params.customerID;
+    const payload: JwtPayload = (req as any).jwtPayload;
+    const customerID = String(payload.sub);
     const productID = req.body.productID;
     await new CreateOrderProductService().execute(customerID, productID);
     res.status(200).json({
@@ -15,19 +17,23 @@ export class OrdersController {
   };
 
   getOrderProducts = async (req: Request, res: Response) => {
-    const customerID = req.params.customerID;
+    const payload: JwtPayload = (req as any).jwtPayload;
+    const customerID = String(payload.sub);
     const orderProducts = await new GetOrderProductsService().execute(customerID);
     res.status(200).json({ data: orderProducts });
   };
 
   deleteOrderProduct = async (req: Request, res: Response) => {
+    const payload: JwtPayload = (req as any).jwtPayload;
+    const customerID = String(payload.sub);
     const orderProductID = req.body.orderProductID;
-    await new DeleteOrderProductService().execute(orderProductID);
+    await new DeleteOrderProductService().execute(customerID, orderProductID);
     res.status(200).json({ message: "Product successfully deleted from order." });
   };
 
   checkout = async (req: Request, res: Response) => {
-    const customerID = req.params.customerID;
+    const payload: JwtPayload = (req as any).jwtPayload;
+    const customerID = String(payload.sub);
     await new OrderCheckoutService().execute(customerID);
     res.status(200).json({ message: "Order sucessfully completed." });
   };
