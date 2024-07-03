310fbd179b112386e6c1816a98af94b772b878d6
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-03 17:44:52 -0300

    feat(authMiddleware): add token refresh handling

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,20 +1,44 @@
 import { NextFunction, Request, Response } from "express";
-import { JwtPayload, verify } from "jsonwebtoken";
+import { JwtPayload, VerifyErrors, verify } from "jsonwebtoken";
+import { JWTHandler } from "../utils/JWTHandler";
+import { getCookies } from "../utils/getCookies";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const authorization = req.headers.authorization;
+  const accessToken = req.headers.authorization;
 
-  if (!authorization) {
+  if (!accessToken) {
     res.status(401).json({ message: "Unauthorized access." });
   }
 
-  const [, token] = String(authorization).split(" ");
+  const token = String(accessToken).split(" ")[1];
   verify(token, String(process.env.TOKEN_SECRET), (err, payload) => {
     if (err) {
-      res.status(403).json({ message: "Forbidden access." });
+      if (err.message == "jwt expired") {
+        const cookies = getCookies(req);
+        const refreshToken = cookies.refreshToken;
+        if (!refreshToken) throw new Error("No Refresh Token provided.");
+
+        verify(refreshToken, String(process.env.REFRESH_TOKEN_SECRET), (e, p) => {
+          const refreshTokenPayload = p as JwtPayload;
+          const tokenData = {
+            userID: String(refreshTokenPayload.userID),
+            userName: String(refreshTokenPayload.userName),
+            userAccessLevel: Number(refreshTokenPayload.userID),
+          };
+
+          const newAccessToken = JWTHandler.generateAccessToken(tokenData);
+          res.header("Authorization", newAccessToken);
+          res.cookie("refreshToken", refreshToken, { httpOnly: true, sameSite: "strict" });
+          res.status(200).json({
+            message: "New Access Token provided.",
+          });
+        });
+      }
+
+      throw new Error(err.message);
     }
 
-    (req as any).jwtPayload = payload as JwtPayload;
+    (req as any).jwtPayload = payload;
   });
 
   next();
