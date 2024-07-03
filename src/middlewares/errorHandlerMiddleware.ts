05953c76d5412f3b5d05b5d210fffec2335b843d
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-03 17:45:56 -0300

    refactor(errorHandlerMiddleware): use getErrorResponse function

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,16 +1,14 @@
 import { NextFunction, Request, Response } from "express";
-import { ValidationError } from "yup";
+import { getErrorResponse } from "../utils/getErrorResponse";
 
-export function errorHandlerMiddleware(err: any, req: Request, res: Response, next: NextFunction) {
-  console.log(err);
-  let message = "Something went wrong.";
+export function errorHandlerMiddleware(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (process.env.NODE_ENV == "development") console.log(err.message);
+  const { status, message } = getErrorResponse(err);
 
-  if (err instanceof ValidationError || err instanceof Error) {
-    err.message ==
-    'new row for relation "products" violates check constraint "products_product_stock_check"'
-      ? (message = "One or more product(s) contained in the order are currently not in stock.")
-      : (message = err.message);
-  }
-
-  return res.status(500).json({ message });
+  return res.status(status).json({ message });
 }
