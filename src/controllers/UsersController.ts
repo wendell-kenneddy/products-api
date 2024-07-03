a165b5d21b536d330ec467968b40861e338b59d7
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-03 17:47:27 -0300

    refactor(UsersController): use validateAccessLevel and rename id variables

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -4,30 +4,11 @@ import { GetOneUserService } from "../services/users/GetOneUserService";
 import { GetManyUsersService } from "../services/users/GetManyUsersService";
 import { queryPageSchema } from "../utils/queryPageSchema";
 import { DeleteOneUserService } from "../services/users/DeleteOneUserService";
-import { JWTHandler } from "../utils/JWTHandler";
-import { ValidateLoginService } from "../services/users/ValidateLoginService";
-import { JwtPayload } from "jsonwebtoken";
+import { validateAccessLevel } from "../utils/validateAccessLevel";
 
 export class UsersController {
-  signup = async (req: Request, res: Response) => {
-    const data = req.body;
-    const user = await new CreateUserService().execute({ userAccessLevel: 1, ...data });
-    const token = JWTHandler.generateAccessToken(user);
-    res.json({ message: "User created successfully.", token });
-  };
-
-  login = async (req: Request, res: Response) => {
-    const data = req.body;
-    const user = await new ValidateLoginService().execute(data);
-    const token = JWTHandler.generateAccessToken(user);
-    res.json({ message: "Login successful.", token });
-  };
-
   create = async (req: Request, res: Response) => {
-    const payload: JwtPayload = (req as any).jwtPayload;
-    const accessLevel = Number(payload.userAccessLevel);
-
-    if (accessLevel < 3) res.status(403).json({ message: "Forbidden access." });
+    validateAccessLevel(3, req);
 
     const data = req.body;
     const { userID, userName, userEmail, userAccessLevel } = await new CreateUserService().execute(
@@ -44,13 +25,12 @@ export class UsersController {
   };
 
   getOne = async (req: Request, res: Response) => {
-    console.log((req as any).jwtPayload);
-    const id = (req as any).jwtPayload.sub;
-    const userWithID = await new GetOneUserService().execute(id);
+    const id = (req as any).jwtPayload.userID;
+    const dbUser = await new GetOneUserService().execute(id);
     let data = {};
 
-    if (userWithID) {
-      const { userID, userName, userEmail, userAccessLevel } = userWithID;
+    if (dbUser) {
+      const { userID, userName, userEmail, userAccessLevel } = dbUser;
       data = { userID, userName, userEmail, userAccessLevel };
     }
 
@@ -60,8 +40,7 @@ export class UsersController {
   };
 
   getMany = async (req: Request, res: Response) => {
-    const accessLevel = Number((req as any).jwtPayload.userAccessLevel);
-    if (accessLevel < 2) res.status(403).json({ message: "Forbidden access." });
+    validateAccessLevel(2, req);
     const queryPage: any = req.body;
     await queryPageSchema.validate(queryPage);
     const condition = `LIMIT ${queryPage.pageSize} OFFSET ${queryPage.offset}`;
@@ -77,8 +56,8 @@ export class UsersController {
   };
 
   deleteOne = async (req: Request, res: Response) => {
-    const id = String((req as any).jwtPayload.sub);
-    await new DeleteOneUserService().execute(id);
+    const userID = String((req as any).jwtPayload.userID);
+    await new DeleteOneUserService().execute(userID);
     return res.status(200).json({
       message: "User deleted successfully.",
     });
