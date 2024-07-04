9fbd62ef2adf3dd27b3f528d6ffebe4f34c02c01
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-04 09:49:09 -0300

    refactor(getErrorResponse): import ERROR_RESPONSES instead of hard codeing it

--- src/utils/getErrorResponse.ts
+++ src/utils/getErrorResponse.ts
@@ -1,23 +1,5 @@
 import { ValidationError } from "yup";
-
-type ErrorResponses = Record<string, [number, string]>;
-
-const ERROR_RESPONSES: ErrorResponses = {
-  "invalid token": [401, "Invalid Access/Refresh Token(s) provided."],
-  "jwt malformed": [401, "Invalid Access/Refresh Token(s) provided."],
-  "jwt expired": [401, "Access/Refresh token expired."],
-  "unauthenticated access": [401, "Unauthenticated acess denied."],
-  "forbidden access": [403, "Unauthorized acess denied."],
-  "login fail": [400, "Invalid email or password."],
-  'new row for relation "products" violates check constraint "products_product_stock_check"': [
-    500,
-    "One or more product(s) contained in the order are currently not in stock.",
-  ],
-  'duplicate key value violates unique constraint "users_user_email_key"': [
-    400,
-    "User with provided email already exists.",
-  ],
-};
+import { ERROR_RESPONSES } from "./ERROR_RESPONSES";
 
 export function getErrorResponse(err: Error) {
   let message = "Server error.";
