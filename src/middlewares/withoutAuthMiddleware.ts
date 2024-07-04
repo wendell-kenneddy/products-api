9120c2687f2f89586a8d3eec7bd33d15aa3a4a73
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-04 09:50:38 -0300

    refactor(withoutAuthMiddleware): delegate error handling to proper middleware

--- src/middlewares/withoutAuthMiddleware.ts
+++ src/middlewares/withoutAuthMiddleware.ts
@@ -2,9 +2,6 @@ import { NextFunction, Request, Response } from "express";
 
 export const withoutAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authorization = req.headers.authorization;
-  const message = "User already signed in.";
-
-  if (authorization) res.status(401).json({ message });
-
+  if (authorization) throw new Error("unauthenticated only");
   next();
 };
