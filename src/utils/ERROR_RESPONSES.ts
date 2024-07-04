4e0d6aeb03bb79c10788972a96e5b9bedf882f88
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-04 09:48:19 -0300

    feat(utils): move ERROR_RESPONSES object to it's own file

---
+++ src/utils/ERROR_RESPONSES.ts
@@ -0,0 +1,19 @@
+type ErrorResponses = Record<string, [number, string]>;
+
+export const ERROR_RESPONSES: ErrorResponses = {
+  "invalid token": [401, "Invalid Access/Refresh Token(s) provided."],
+  "jwt malformed": [401, "Invalid Access/Refresh Token(s) provided."],
+  "jwt expired": [401, "Access/Refresh token expired."],
+  "unauthenticated access": [401, "Unauthenticated access denied."],
+  "forbidden access": [403, "Unauthorized acess denied."],
+  "unauthenticated only": [401, "User already signed in."],
+  "login fail": [400, "Invalid email or password."],
+  'new row for relation "products" violates check constraint "products_product_stock_check"': [
+    500,
+    "One or more product(s) contained in the order are currently not in stock.",
+  ],
+  'duplicate key value violates unique constraint "users_user_email_key"': [
+    400,
+    "User with provided email already exists.",
+  ],
+};
